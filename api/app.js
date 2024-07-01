172d43d088397ec2232039057823d422021398dc
SADC GIP <gip@sadc-gmi.org>
2024-07-01 08:09:11 +0200

    01 july

--- api/app.js
+++ api/app.js
@@ -1,6 +1,15 @@
 import express from "express";
+import router from  "./routes/post.route.js"
 const app = express();
+
+
+app.use("/api/test", (req,res)=> {
+res.send("it works")
+})
 
 app.listen(8800, ()=> {
     console.log ("Server is running");
 })
+
+
+
